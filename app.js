8c2eca58f3b41665f713b49e185fd933c500b0c8
Alex Junior Barreto <alexjuniorbarreto@gmail.com>
2023-03-16 12:26:32 -0300

    tt

--- app.js
+++ app.js
@@ -65,12 +65,19 @@ function cadastrarDespesa() {
 
         document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
         document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso'
+        document.getElementById('modal_btn').innerHTML = 'Voltar'
+        document.getElementById('modal_btn').className = 'btn btn-success'
 
         $('#modalRegistroDespesa').modal('show')
         //
     } else {
         document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
         document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+        document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique todos os campos foram preenchidos corretamente'
+        document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+        document.getElementById('modal_btn').className = 'btn btn-danger'
+
 
         $('#modalRegistroDespesa').modal('show')
     }
